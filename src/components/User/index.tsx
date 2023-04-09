f91127259b8d833d799de09530523938e3991151
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2023-04-09 15:26:09 +0800

    tutorial eleven

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -7,7 +7,7 @@ type AuthUser = {
 };
 
 export default function User() {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<AuthUser>({} as AuthUser);
 
   const handleLogin = () => {
     setUser({
@@ -15,17 +15,17 @@ export default function User() {
       email: "vishwas@example.com",
     });
   };
-  
-  const handleLogout = () => {
-    setUser(null);
-  };
+
+  // const handleLogout = () => {
+  //   setUser(null);
+  // };
 
   return (
     <div className={styles["user"]}>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   );
 }
