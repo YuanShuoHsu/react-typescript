55fd74bce3d0c204ecefa4b89bc8732e88d43152
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2023-04-10 14:10:34 +0800

    tutorial 17

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Box from "./components/context/Box";
 import ThemeContextProvider from "./components/context/ThemeContext";
 import UserContextProvider from "./components/context/UserContext";
 import User from "./components/state/User";
+import Counter from "./components/class/Counter";
 
 function App() {
   const personName = {
@@ -56,6 +57,7 @@ function App() {
       <UserContextProvider>
         <User />
       </UserContextProvider>
+      <Counter message="The count value is"></Counter>
     </div>
   );
 }
