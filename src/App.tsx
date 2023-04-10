9b543b547eccf19cd2bb9722a345ee07d7386873
YuanShuoHsu <79447837+YuanShuoHsu@users.noreply.github.com>
2023-04-10 15:24:09 +0800

    tutorial 20

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import User from "./components/state/User";
 import Counter from "./components/class/Counter";
 import Private from "./components/auth/Private";
 import Profile from "./components/auth/Profile";
+import List from "./components/generics/List";
+import RandomNumber from "./components/restriction/RandomNumber";
 
 function App() {
   const personName = {
@@ -61,6 +63,20 @@ function App() {
       </UserContextProvider>
       <Counter message="The count value is" />
       <Private isLoggedIn={true} component={Profile} />
+      {/* <List
+        items={["Batman", "Superman", "Wonder Woman"]}
+        onClick={(item) => console.log(item)}
+      />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          { id: 1, first: "Bruce", last: "Wayne" },
+          { id: 2, first: "Clark", last: "Kent" },
+          { id: 3, first: "Princess", last: "Diana" },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
+      <RandomNumber value={10} isPositive />
     </div>
   );
 }
